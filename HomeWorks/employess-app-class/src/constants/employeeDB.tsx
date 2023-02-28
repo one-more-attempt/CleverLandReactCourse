a8fb026a9c4a82f44caa3903096d874d1fd56a79
1more.attempt <echo.042014@gmail.com>
2023-02-28 20:31:29 +0300

    rewrite all to class components

---
+++ HomeWorks/employess-app-class/src/constants/employeeDB.tsx
@@ -0,0 +1,59 @@
+import type { EmployeeListTypes } from "../types/types";
+export const employeeDB: EmployeeListTypes[] = [
+    {
+      id: 1,
+      name: `John Dorian`,
+      salary: 1100,
+      salaryBonus: false,
+      onRise: false,
+    },
+    {
+      id: 2,
+      name: `Hassan Schultz`,
+      salary: 1200,
+      salaryBonus: true,
+      onRise: false,
+    },
+    {
+      id: 3,
+      name: `Anisa Bryant`,
+      salary: 80,
+      salaryBonus: true,
+      onRise: false,
+    },
+    {
+      id: 4,
+      name: `John Dorian`,
+      salary: 52000,
+      salaryBonus: false,
+      onRise: false,
+    },
+    {
+      id: 5,
+      name: `Syed Harding`,
+      salary: 1340,
+      salaryBonus: true,
+      onRise: true,
+    },
+    {
+      id: 6,
+      name: `Magnus Mills`,
+      salary: 400,
+      salaryBonus: false,
+      onRise: false,
+    },
+    {
+      id: 7,
+      name: `Otto Elliott`,
+      salary: 200,
+      salaryBonus: false,
+      onRise: false,
+    },
+    {
+      id: 8,
+      name: `Sarah Pratt`,
+      salary: 134,
+      salaryBonus: false,
+      onRise: false,
+    },
+  ];
